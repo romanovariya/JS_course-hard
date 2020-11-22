77c917e93ac4497475ebf83a3560caf17d4e672b
Riya Romanova <riyaromanova13@gmail.com>
2020-11-22 18:27:24 +0300

    fix(script.js): fixed mistakes

--- js/script.js
+++ js/script.js
@@ -1,27 +1,20 @@
 const num = 266219;
 
 let strNum,
-    slice1,
-    slice2,
-    slice3,
-    slice4,
-    slice5,
-    slice6,
+    res = 1,
+    len,
     cube,
+    i = 0,
     partOfCube;
 
 strNum = num.toString();
-slice1 = strNum.slice(0, 1);
-slice2 = strNum.slice(1, 2);
-slice3 = strNum.slice(2, 3);
-slice4 = strNum.slice(3, 4);
-slice5 = strNum.slice(4, 5);
-slice6 = strNum.slice(5, 6);
-cube = +slice1 * +slice2 * +slice3 * +slice4 * +slice5 * +slice6;
-
-console.log(cube);
-
-cube = cube ** 3;
+len  = strNum.length;
+while (i < len){
+    res *= +strNum[i];
+    i++;
+}
+console.log(res);
+cube = res ** 3;
 
 partOfCube = String(cube).slice(0, 2);
 console.log(partOfCube);
