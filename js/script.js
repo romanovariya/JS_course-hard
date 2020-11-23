924418a1118be0b215962acac028e540fba6c08a
Riya Romanova <riyaromanova13@gmail.com>
2020-11-23 21:26:55 +0300

    fix(script.js): add second part

--- js/script.js
+++ js/script.js
@@ -4,21 +4,27 @@ const lang = prompt('ru or en'),
     weekArray = {
         ru: [weekRu],
         en: [weekEn],
-    };
-if (lang !== 'ru' && lang !== 'en') {
-    alert('Произошла ошибка. Перезагрузите страницу');
-}
-if (lang === 'ru') {
+    },
+    namePerson = prompt('Введите имя?');
+if (lang === 'ru' || lang === 'en') {
+   if (lang === 'ru') {
     console.log(weekRu);
-} else if (lang === 'en') {
+    } else if (lang === 'en') {
     console.log(weekEn);
+    }
+    switch (lang) {
+        case 'ru':
+            console.log(weekRu);
+            break;
+        case 'en':
+            console.log(weekEn);
+            break;
+    }
+    console.log(weekArray[lang]);
+} else {
+ alert('Произошла ошибка. Перезагрузите страницу');
 }
-switch (lang) {
-    case 'ru':
-        console.log(weekRu);
-        break;
-    case 'en':
-        console.log(weekEn);
-        break;
-}
-console.log(weekArray[lang]);
+let job = (namePerson === "Артем") ? 
+    "директор" : (namePerson === "Максим" ? 
+        "преподаватель" : "студент");
+console.log(job);
