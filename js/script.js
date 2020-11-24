a53e4e3e6a50537ad6238b896e56ea14d8698723
Riya Romanova <riyaromanova13@gmail.com>
2020-11-25 00:36:15 +0300

    feat(lesson04): added hard task

--- js/script.js
+++ js/script.js
@@ -1,30 +1,13 @@
-const lang = prompt('ru or en'),
-    weekRu = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятнца", "Суббота", "Воскресенье"],
-    weekEn = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-    weekArray = {
-        ru: weekRu,
-        en: weekEn,
-    },
-    namePerson = prompt('Введите имя?');
-if (lang === 'ru' || lang === 'en') {
-   if (lang === 'ru') {
-    console.log(weekRu);
-    } else if (lang === 'en') {
-    console.log(weekEn);
+'use strict';
+const myFunc = function(arg) {
+    let newArg;
+    if (typeof(arg) !== 'string'){
+        console.log('Введите строку');
+    } else {
+        newArg = arg.trim();
+        if(newArg.length > 30) {
+            newArg = newArg.substring(0, 30) + '...';
+        }
     }
-    switch (lang) {
-        case 'ru':
-            console.log(weekRu);
-            break;
-        case 'en':
-            console.log(weekEn);
-            break;
-    }
-    console.log(weekArray[lang]);
-} else {
- alert('Произошла ошибка. Перезагрузите страницу');
-}
-let job = (namePerson === "Артем") ? 
-    "директор" : (namePerson === "Максим" ? 
-        "преподаватель" : "студент");
-console.log(job);
+    return newArg;
+};
